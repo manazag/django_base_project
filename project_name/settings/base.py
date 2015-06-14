d3a4c2d800146c8e0769ae67e33eb2a952066b92
Bruno Santeramo <bruno.santeramo@gmail.com>
2015-06-14 22:48:49 +0200

    set DEBUG in settings

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -38,14 +38,9 @@ def get_env_variable(var_name):
  
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
